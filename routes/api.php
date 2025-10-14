b98bd28eac4fe5e2c24d4500bdaf093fab814825
unknown <koneabou669@gmail.com>
2025-10-14 11:19:51 +0000

    initialisation projet

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,6 @@ use App\Http\Controllers\AuthController;
     return response()->json(['ping' => 'pong']);
 }); */
 
-Route::post('/register', [AuthController::class, 'register']); // Route pour l'inscription
-Route::post('/login', [AuthController::class, 'login']);// Route pour la connexion
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');// Route pour la déconnexion, protégée par le middleware auth:sanctum
-// Route protégée pour obtenir les informations de l'utilisateur authentifié
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) { 
-    return $request->user();
-});
+
 
 
